6ce3e00b22de635e8698f6c25ad02790886a554a
Kathee <Kathee@example.org>
2023-04-04 21:12:32 +0200

    Dictionary Ecosystem{} with all plants and stats

--- PlantManager.gd
+++ PlantManager.gd
@@ -5,9 +5,33 @@ signal to_inventory
 var Planta = preload("res://src/Actors/Plant.tscn")
 # Diccionari = {"id" : ["nom planta", HP inicial, "tipus", "color"],...} 
 var Ecosystem = {
-	0:{name = "Bleda", hp = 70, type = "vegetable", color = "green"}, 
-	1:{name = "Tomàquet", hp = 50, type = "vegetable", color = "red"}, 
-	2:{name = "Col", hp = 60, type = "vegetable", color = "blue"}
+	0:{name = "Chard", water = 5, sun = 5, type = "crop", color = "green"}, 
+	1:{name = "Tomato", water = 5, sun = 7.5, type = "crop", color = "red"}, 
+	2:{name = "Corn", water = 5, sun = 7.5, type = "crop", color = "blue"},
+	3:{name = "Eggplant", water = 7, sun = 10.5, type = "crop", color = "purple"},
+	4:{name = "Potato", water = 7, sun = 10.5, type = "crop", color = "yellow"},
+	5:{name = "Pumpkin", water = 7, sun = 10.05, type = "crop", color = "orange"},
+	6:{name = "Cabbage", water = 9, sun = 13.5, type = "crop", color = "turquoise"},
+	7:{name = "Truffle", water = 9, sun = 13.5, type = "crop", color = "black"},
+	8:{name = "Turnip", water = 10, sun = 15, type = "crop", color = "white"},
+	9:{name = "Mum", water = 5, sun = 5, type = "flower", color = "green"}, 
+	10:{name = "Rose", water = 5, sun = 5, type = "flower", color = "red"}, 
+	11:{name = "Forget", water = 5, sun = 5, type = "flower", color = "blue"},
+	12:{name = "Violet", water = 7, sun = 7, type = "flower", color = "purple"},
+	13:{name = "Daffodil", water = 7, sun = 7, type = "flower", color = "yellow"},
+	14:{name = "Hibiscus", water = 7, sun = 7, type = "flower", color = "orange"},
+	15:{name = "Hyacynth", water = 9, sun = 9, type = "flower", color = "turquoise"},
+	16:{name = "Tulip", water = 9, sun = 9, type = "tree", color = "black"},
+	17:{name = "Daisy", water = 10, sun = 10, type = "tree", color = "white"},
+	18:{name = "Pear", water = 5, sun = 10, type = "tree", color = "green"}, 
+	19:{name = "Cherry", water = 5, sun = 7.5, type = "tree", color = "red"}, 
+	20:{name = "Blueberry", water = 5, sun = 10, type = "tree", color = "blue"}, #TODO CHANGE COLOR OF SPRITE
+	21:{name = "Grape", water = 7, sun = 14, type = "tree", color = "purple"},
+	22:{name = "Lemon", water = 7, sun = 14, type = "tree", color = "yellow"},
+	23:{name = "Mandarine", water = 7, sun = 14, type = "tree", color = "orange"},
+	24:{name = "Figtree", water = 9, sun = 18, type = "tree", color = "turquoise"}, #TODO CHANGE COLOR OF SPRITE
+	25:{name = "Blackberry", water = 9, sun = 18, type = "tree", color = "black"},
+	26:{name = "Apple", water = 10, sun = 20, type = "tree", color = "white"},
 }
 # crear diccionari on s'emmagatzemin les plantes creades
 var next_id = 0
@@ -27,9 +51,9 @@ func create_plant():
 	var nova_planta = Planta.instance() # Afegeix una nova planta a la llista de plantes existents
 	var selected_plant = randi() % Ecosystem.size() #tria un numero random del 0 al (total de plantes existents)
 	nova_planta.set_position(Vector2(rand_range(-200,200), rand_range(-100, 200)))
-	nova_planta.initialize(Ecosystem[selected_plant], next_id)
 	habitat[next_id] = nova_planta #afegim la nova planta a la llista de plants
 	add_child(nova_planta)
+	nova_planta.initialize(Ecosystem[selected_plant], next_id)
 	nova_planta.connect("harvested", self, "on_plant_harvested", [next_id])
 	next_id += 1
 	
--- src/Actors/Plant.gd
+++ src/Actors/Plant.gd
@@ -5,11 +5,11 @@ signal harvested
 export var num_crop = 0 #variable global -> Compta el total de plantes des de l'inici
 var num_water = 0 #variable local
 var max_hp = 100 
-var tex2 = preload("res://Assets/player2.png")
-var tex3 = preload("res://Assets/player3.png")
-onready var Player_sprite = $PlayerSprite #get_node("PlayerSprite")
+var textures = []
+onready var Plant_sprite = $PlantSprite #get_node("PlantSprite")
 var name_plant
-var hp
+var water # nombre de clicks
+var sun # delta de temps per passar d'stage
 
 func _ready():
 	connect("clicked",self, "on_watered")
@@ -19,7 +19,10 @@ func initialize(plant_data, plant_id):
 	self.hp = plant_data["hp"] #
 	self.name_plant = plant_data["name"] #
 	self.num_crop = plant_id #Es una script variable que cal exportar!
-	
+	tex1 = load("res://Assets/Plants/" + name_plant + "_1.png")
+	tex2 = load("res://Assets/Plants/" + name_plant + "_2.png")
+	tex3 = load("res://Assets/Plants/" + name_plant + "_3.png")
+	tex4 = load("res://Assets/Plants/" + name_plant + "_4.png")
 	
 func on_watered():
 	num_water += 1 #DEBUG counter
@@ -27,11 +30,15 @@ func on_watered():
 	hp += 10 #increase value of hp by 10 points
 	if hp <= 0:
 		print("Error en ", name_plant, " ", num_crop, "HP NEGATIU") #DEBUG
-	elif hp == 80:
-		Player_sprite.texture = tex2
+	elif hp <= 50:
+		Plant_sprite.texture = tex1
+	elif hp <= 60:
+		Plant_sprite.texture = tex2
+	elif hp <= 80:
+		Plant_sprite.texture = tex3
 	elif hp == max_hp:
 		print(name_plant, " ", num_crop, " is fully grown!")
-		Player_sprite.texture = tex3
+		Plant_sprite.texture = tex4
 	elif hp > max_hp:
 		print(name_plant, " ", num_crop, " harvested!")
 		emit_signal("harvested")
