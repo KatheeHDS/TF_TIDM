16d23f583b6410c32b723a9bd8abad3dc82726c2
Kathee <Kathee@example.org>
2023-04-04 18:18:16 +0200

    Added v1 art, next TODO plant manager tasks set

--- PlantManager.gd
+++ PlantManager.gd
@@ -26,9 +26,8 @@ func _process(delta):
 func create_plant():
 	var nova_planta = Planta.instance() # Afegeix una nova planta a la llista de plantes existents
 	var selected_plant = randi() % Ecosystem.size() #tria un numero random del 0 al (total de plantes existents)
-	nova_planta.hp = Ecosystem[selected_plant]["hp"] #
-	nova_planta.name_plant = Ecosystem[selected_plant]["name"] #
-	nova_planta.num_crop = next_id #Es una script variable que cal exportar!
+	nova_planta.set_position(Vector2(rand_range(-200,200), rand_range(-100, 200)))
+	nova_planta.initialize(Ecosystem[selected_plant], next_id)
 	habitat[next_id] = nova_planta #afegim la nova planta a la llista de plants
 	add_child(nova_planta)
 	nova_planta.connect("harvested", self, "on_plant_harvested", [next_id])
@@ -43,3 +42,11 @@ func on_plant_harvested(id):
 	habitat.erase(id) # elimina la planta del diccionari
 	# gestionar inventari: aquí o en funció dedicada? -> Enviar signal a un altre script. 
 	# Caldrà que manager i inventari siguin germans i hi hagi un pare que els gestioni
+
+
+# TODO
+# - crear una variable de classe que sigui una array de textures
+# - Omplir aquesta array a la funció initialize amb les 4 textures de la planta
+# > load() NO PRELOAD!
+# - Posar sprite del player_sprite a la primera textura (L10 plant.gd)
+# - en la funcio de creixement, quan detecti que una planta canvia d'stage, canviar sprite
--- src/Actors/Plant.gd
+++ src/Actors/Plant.gd
@@ -2,30 +2,36 @@ extends Actor
 
 signal harvested
 
-export var num_crop = 0 #variable global
-export var type_crop = "P" # P = Vegetable, A = Tree, F = Flower
+export var num_crop = 0 #variable global -> Compta el total de plantes des de l'inici
 var num_water = 0 #variable local
 var max_hp = 100 
-var hp = 70 #hp inicial
 var tex2 = preload("res://Assets/player2.png")
 var tex3 = preload("res://Assets/player3.png")
 onready var Player_sprite = $PlayerSprite #get_node("PlayerSprite")
 var name_plant
+var hp
 
 func _ready():
 	connect("clicked",self, "on_watered")
+
+func initialize(plant_data, plant_id):
+	print(plant_data)
+	self.hp = plant_data["hp"] #
+	self.name_plant = plant_data["name"] #
+	self.num_crop = plant_id #Es una script variable que cal exportar!
+	
 	
 func on_watered():
 	num_water += 1 #DEBUG counter
-	print(type_crop, " ", num_crop , " regada " , num_water , " vegades")
+	print(name_plant, " ", num_crop , " regada " , num_water , " vegades. HP ", hp)
 	hp += 10 #increase value of hp by 10 points
 	if hp <= 0:
-		print("Error en ", type_crop, " ", num_crop, "HP NEGATIU") #DEBUG
+		print("Error en ", name_plant, " ", num_crop, "HP NEGATIU") #DEBUG
 	elif hp == 80:
 		Player_sprite.texture = tex2
 	elif hp == max_hp:
 		print(name_plant, " ", num_crop, " is fully grown!")
 		Player_sprite.texture = tex3
 	elif hp > max_hp:
-		print(type_crop, " ", num_crop, " harvested!")
+		print(name_plant, " ", num_crop, " harvested!")
 		emit_signal("harvested")
